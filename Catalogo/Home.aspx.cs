5e56b7ddf0f2ef085bb4a09ea775916ffd063a8f
camilaViquez <camila.viquez87@gmail.com>
2018-06-03 09:17:45 -0600

    Web server

---
+++ Catalogo/Home.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Home : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Consumo del web service
+        WebServiceDB.BDSoapClient webService = new WebServiceDB.BDSoapClient();
+        DataSet dataSet = webService.GetData();
+        GridView1.DataSource = dataSet.Tables[0];
+        GridView1.DataBind();
+    }
+
+
+
+    //Dejar esto aca no quitar
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ WebServiceDB/App_Code/BD.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+//acceso a base de datos
+using System.Data.SqlClient;
+
+
+/// <summary>
+/// Descripción breve de BD
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+// [System.Web.Script.Services.ScriptService]
+public class BD : System.Web.Services.WebService
+{
+
+    public BD()
+    {
+
+        //Elimine la marca de comentario de la línea siguiente si utiliza los componentes diseñados 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hola a todos";
+    }
+
+    //Metodo para la coneccion con nustra base de datos
+    [WebMethod]
+    public DataSet GetData()
+    {
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = "Data Source=LAPTOP-MKRNOBFU; Initial Catalog=Macronals_DB; Integrated Security=True;";
+        SqlDataAdapter adaptador = new SqlDataAdapter("SELECT * FROM Persona", con);
+        //para recibir/mostrar datos de consulta
+        DataSet dataSet = new DataSet();
+        adaptador.Fill(dataSet);
+        return dataSet;
+
+    }
+
+
+}
